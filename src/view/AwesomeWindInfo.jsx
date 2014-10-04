299c76c7bf83eac56492f47edd8a18597a658178
Wietse Venema <wietse@finalist.com>
2014-10-04 12:10:57 +0200

    Awesome wind rotated

--- src/view/AwesomeWindInfo.jsx
+++ src/view/AwesomeWindInfo.jsx
@@ -1,21 +1,32 @@
 require("./AwesomeWindInfo.styl");
 var Model = require("model/Model");
 
+
 var AwesomeWindInfo = React.createClass({
     propTypes: {
         data: React.PropTypes.instanceOf(Model.CurrentWeather)
     },
 
+
+
     render() {
         if(!this.props.data.hasData()) {
             return <div className="AwesomeWindInfo">Loading!</div>;
-        }
+        }   
+        var height = Math.round(this.props.data.wind.speed*10);
+
+        var style = {
+            height: height,
+            width: "100%",
+            background: "red",
+            transform: "rotate("+this.props.data.wind.deg+"deg)"
+        };
 
         return (
             <div className="AwesomeWindInfo">
                 <h2>Awesome wind in {this.props.data.name}</h2>
-                <p>{this.props.data.wind.speed}!</p>
-                <p>{this.props.data.wind.deg}!</p>
+                <div style={style}></div>
+                
             </div>
         );
     }
